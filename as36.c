62bc2801acf1e8be4b65b671aa48825b62700447
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-05 16:14:57 +0530

    Create as36.c

---
+++ as36.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,t;
+printf("enter the values");
+scanf("%d","%d",&a,&b);
+while(n<0)
+{
+t=a;
+a=b;
+b=t;
+printf('the swap value of a is%d"a);
+printf("the swap value of b is%d",b);
+}
+getch();
+}
