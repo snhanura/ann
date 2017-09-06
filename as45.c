2aecef4a942ef171633c6c17096a9eaf11a775d5
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-06 16:51:38 +0530

    Create as45.c

---
+++ as45.c
@@ -0,0 +1,12 @@
+#includ<stdio.h>
+void main()
+{
+int a[100];
+printf("enter the number between specified range");
+scanf("%d',&a[100]);
+for(a[100]>=99)
+{
+printf("you enter the number not in the range");
+}
+getch();
+]
