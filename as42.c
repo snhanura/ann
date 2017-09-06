99e37436279922d39a5e800b6ae411a2aeecf4d6
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-06 16:30:30 +0530

    Update as42.c

--- as42.c
+++ as42.c
@@ -5,3 +5,15 @@ char s1[50],s2[50];
 int i;
 printf("enter the 2 string values");
 scanf("%c%c",&s1[50],s2[50]);
+  if(str1[i]!='\0'&&str2[i]!='\0')
+  {
+   if(str1[i]==str2[i])
+   {
+    printf("given string are egual");
+   }
+    else
+    {
+     printf("given string are not equal");
+    }
+    getch();
+  }
