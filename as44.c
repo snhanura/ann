ed1a79f92740ade5fe63aa39e074ea8ba3b28816
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-06 16:48:23 +0530

    Create as44.c

---
+++ as44.c
@@ -0,0 +1,12 @@
+#includ<stdio.h>
+void main()
+{
+int a[10];
+printf("enter the number between 1-9");
+scanf("%d',&a[10]);
+for(a[10]>=9)
+{
+printf("you enter teh number not in the range");
+}
+getch();
+]
