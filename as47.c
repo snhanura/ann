af8ddd3d3a2abcbd21536ed0218b645436e05b6a
snhanura <31534294+snhanura@users.noreply.github.com>
2017-09-08 16:16:16 +0530

    Create as47.c

---
+++ as47.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+void main()
+{
+int a[100],n;
+printf("enter the n value");
+scanf("%d",&n);
+for(i=0;a[i]<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<a[i];i++)
+{
+if(a[i]>a[0])
+{
+hi=a[i];
+}
+printf("the highest value is%d",a[i]);
+if(a[i]<a[0])
+{
+low=a[i];
+}
+printf(" the lowest value is%d",a[i]);
+}
